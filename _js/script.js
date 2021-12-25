c597ee4a265add419f7f304f81228cab929cd61c
Demetrrius <demein@gmail.com>
2021-12-25 17:45:14 -0300

    Confirando Js

--- _js/script.js
+++ _js/script.js
@@ -7,17 +7,25 @@ const linksSocialMedia ={
 }      
 function changeSocialMediaLinks() {  
   for (let li of socialLinks.children) {
-     const social = li.getAttribute("class")
+    const social = li.getAttribute("class")
 
     li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}` 
-    alert(li.children[0].href)
+    
   }    
 }      
 changeSocialMediaLinks()       
 
 function getGitHubProfileInfos(){
   const url = `https://api.github.com/users/${linksSocialMedia.github}`   
-}
-  
-fetch(url)
+  fetch(url)        
+  .then(response => response.json()) 
+  .then(data =>{
+    userName.textContent = data.name,
+    userBio.textContent = data.bio,
+    userLink.href = data.html_url,
+    userImage.src = data.avatar_Url,
+    userLogin.textContent = data.login          
+  })
+}       
+
 getGitHubProfileInfos()
